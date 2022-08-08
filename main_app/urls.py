4d23ec2be21620d7f70c0111d7bca675ad44b87c
Erodri6z <erikrodriguez2417@gmail.com>
2022-08-08 04:23:13 -0500

    impliment full crud for toys

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
   path('finches/<int:pk>/delete/', views.FinchDelete.as_view(), name='finches_delete'),
   path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding'),
   path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+  path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+  path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import Finch, Toy
 from .forms import FeedingForm
 
@@ -49,3 +50,16 @@ class ToyCreate(CreateView):
   model = Toy
   fields = '__all__'
 
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
